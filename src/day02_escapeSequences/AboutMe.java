64a2cb97068b505e718861636d5e773a86a0fe9e
omerd1010 <omerd2810@gmail.com>
2023-04-30 09:31:07 +0200

    Day02

---
+++ src/day02_escapeSequences/AboutMe.java
@@ -0,0 +1,27 @@
+package day02_escapeSequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("\tMy favorite book is \"The Rich Dad and The Poor Dad\"\n\tMy favorite quote is \"No Pain No Gain\"\n" +
+                "\tMy favoriteTV show is \"The Game of Thrones\"\n\tMy favorite music is Rock Music");
+
+
+    }
+
+
+}
+
+/*
+1. Create a class named AboutMe.java
+2. Write a code to print the following info about you:
+1. Your favorite book
+2. Your favorite quote
+3. Your favorite TV show
+4. Your favorite music
+
+ */
+
+
+
---
+++ src/day02_escapeSequences/EmployeeInfo.java
@@ -0,0 +1,30 @@
+package day02_escapeSequences;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Name:  Daniel");
+        System.out.println("Gender:  Male");
+        System.out.println("Age:  26");
+        System.out.println("Job Title:  Java Developer");
+        System.out.println("Company name:  Apple Inc");
+        System.out.println("Salary:  $95000");
+
+
+    }
+
+
+}
+
+// single line comment
+
+/*
+    Comment1
+    Comment2
+    Comment3
+    ...
+    Multi-line comments
+
+ */
