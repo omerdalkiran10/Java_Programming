cb0860b9f1d55b5f02f1799937ef44dafc60711c
omerd1010 <omerd2810@gmail.com>
2023-05-03 09:29:59 +0200

    Day04

---
+++ src/day04_concatenation/ConcatenationIntro.java
@@ -0,0 +1,26 @@
+package day04_concatenation;
+
+public class ConcatenationIntro {
+
+    public static void main(String[] args) {
+
+        String name = "Daniel";
+
+        int age = 33;
+
+        int salary = 85000;
+
+        System.out.println("Hello " + name + ".");
+        System.out.println("You are " + age + " years old.");
+        System.out.println("Your salary is " + salary + " USD");
+
+
+        int number = 100_000_000;
+
+        System.out.println("number = " + number);
+
+
+    }
+
+
+}
---
+++ src/day04_concatenation/PersonalInfo.java
@@ -0,0 +1,54 @@
+package day04_concatenation;
+
+public class PersonalInfo { // WarmUp
+
+    public static void main(String[] args) {
+
+        String firstName = "Ömer",
+                lastName = "Dalkiran",
+                from = "Turkey",
+                favoriteMusic = "Pop",
+                favorite_book = "Data Structures in Java",
+                favoriteTvShow = "HIMYM",
+                favoriteColor = "Blue";
+
+
+        char gender = 'M';
+        int age = 31;
+
+        boolean isMarried = true,
+                isEmployed = false;
+
+
+        System.out.println("firstName = " + firstName);
+        System.out.println("lastName = " + lastName);
+        System.out.println("gender = " + gender);
+        System.out.println("age = " + age);
+        System.out.println("from = " + from);
+        System.out.println("favoriteMusic = " + favoriteMusic);
+        System.out.println("favorite_book = " + favorite_book);
+        System.out.println("favoriteTvShow = " + favoriteTvShow);
+        System.out.println("favoriteColor = " + favoriteColor);
+        System.out.println("isMarried = " + isMarried);
+        System.out.println("isEmployed = " + isEmployed);
+
+
+    }
+
+
+}
+/*
+Warmup task:
+	1. Create a class named PersonalInfo, and declare the variables with the appropriate for the followings:
+			1. first name
+			2. last name
+			3. gender
+			4. age
+			5. from
+			6. favorite music
+			7. favorite book
+			8. favorite show
+			9. favorite color
+			10. married status
+			11. employee status
+ */
