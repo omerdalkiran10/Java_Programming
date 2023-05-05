881f9cff1eb428bc8566d3a42ac16244095b34ea
omerd1010 <omerd2810@gmail.com>
2023-05-05 09:40:40 +0200

    Day06

---
+++ src/day06_ifStatements/Task1_Cigarettes.java
@@ -0,0 +1,32 @@
+package day06_ifStatements;
+
+public class Task1_Cigarettes { // Task 1
+
+    public static void main(String[] args) {
+
+        int age = 45;
+
+        if (age >= 21 && age < 100){
+            System.out.println("You are eligible to buy cigarettes");
+        }
+
+
+        if(age < 21 || age > 100){
+            System.err.println("You are not eligible to buy cigarettes");
+        }
+
+
+
+
+    }
+
+}
+
+/*
+1. Create a class named Cigarettes.java
+2. A variable named age is declared and given, Write a program that can check if the person is eligible to buy cigarettes
+	Ex:
+		age = 20
+	     output:
+		    You are not eligible to buy Cigarettes
+ */
