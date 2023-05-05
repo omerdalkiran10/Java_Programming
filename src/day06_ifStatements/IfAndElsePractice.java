6ea4be62eefb6d8d2ebb9b9b94e868244a7dc06d
omerd1010 <omerd2810@gmail.com>
2023-05-05 09:39:24 +0200

    Day06

---
+++ src/day06_ifStatements/IfAndElsePractice.java
@@ -0,0 +1,23 @@
+package day06_ifStatements;
+
+public class IfAndElsePractice {
+
+    public static void main(String[] args) {
+
+        int score = 40;
+
+
+        if( score >= 60 ){
+            System.out.println("Passed");
+        }else {
+            System.out.println("Failed");
+        }
+
+
+
+
+    }
+
+
+
+}
