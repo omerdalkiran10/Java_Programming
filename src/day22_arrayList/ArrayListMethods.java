063a99df638f6412da441d47dad64787b056825a
omerd1010 <omerd2810@gmail.com>
2023-06-10 09:03:09 +0200

    Java Programming Day22

--- src/day22_arrayList/ArrayListMethods.java
+++ src/day22_arrayList/ArrayListMethods.java
@@ -3,6 +3,7 @@ package day22_arrayList;
 import java.util.ArrayList;
 
 public class ArrayListMethods {
+
     public static void main(String[] args) {
 
         ArrayList<Integer> list = new ArrayList<>();
