cf243d67388017a988cd42244ab57dc548215f57
omerd1010 <omerd2810@gmail.com>
2023-06-10 09:03:34 +0200

    Java Programming Day22

--- src/day22_arrayList/Test.java
+++ src/day22_arrayList/Test.java
@@ -3,6 +3,7 @@ package day22_arrayList;
 import utilities.ArraysUtility;
 
 public class Test {
+
     public static void main(String[] args) {
 
         int[] numbers = {10, 20, 30, 40, 50, 60, 70, 90};
--- src/day22_arrayList/Warmup_MaxMinNumbersOfMultiDArray.java
+++ src/day22_arrayList/Warmup_MaxMinNumbersOfMultiDArray.java
@@ -32,7 +32,6 @@ public class Warmup_MaxMinNumbersOfMultiDArray {
 
 
     }
-
 }
 
 /*
