d0693d4696bb68a620a6d2cd10b672681b7edb3f
omerd1010 <omerd2810@gmail.com>
2023-06-20 08:40:24 +0200

    Java Programming Day27

---
+++ src/day27_accessModifiers/TestNestedClassObjects.java
@@ -0,0 +1,21 @@
+package day27_accessModifiers;
+
+public class TestNestedClassObjects {
+
+    public static void main(String[] args) {
+
+        Car obj1 = new Car(); // Car object
+        Car.CarEngine obj2 = obj1.new CarEngine(); // CarEngine object
+        // You can`t create Car Engine without Car object
+
+        Car.StaticInnerClass obj3 =  new Car.StaticInnerClass();
+        // This is an object of inner static class..
+
+
+
+
+
+
+    }
+
+}
